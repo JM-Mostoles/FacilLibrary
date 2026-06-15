fa9e7087e4cac717902a30d682ac1af562d2461f
Julio <juliomontero.mostoles@gmail.com>
2026-06-15 15:02:02 +0200

    Textbox partially finished

--- main.lua
+++ main.lua
@@ -1,20 +1,22 @@
 function love.load()
-    require("modules.textMaker")
-    hola = textbox_class.new(true, 10, {"aaaa", "bbbb"}, 1, 1, 1, 1)
+	require("modules.textMaker")
+	hola = textbox_class.new(false, 10, { "aaaaaaaaaaaa", "bbbbbbbbbbbb" }, 1, 1, 1, 1)
 end
 
 function love.update(dt)
-    hola:update(dt)
+	hola:update(dt)
 end
 
 function love.keypressed(key)
-    hola:keypressed(key)
-    if key == "a" then
-        hola:setIn(true)
-    end
+	hola:keypressed(key)
+	if hola.isIn == false then
+		if key == "a" then
+			hola:setIn(true)
+		end
+	end
 end
 
 function love.draw()
-    love.graphics.draw(love.graphics.newImage("rs/general/bg.png"))
-    hola:draw(dt)
+	love.graphics.draw(love.graphics.newImage("rs/general/bg.png"))
+	hola:draw(dt)
 end
--- modules/textMaker.lua
+++ modules/textMaker.lua
@@ -1,25 +1,25 @@
 textbox_class = {
 	isIn = false,
-	font = love.graphics.newFont("rs/fonts/earlyGameBoy.ttf", 8),
+	X = (256 / 2),
+	Y = (224 / 2),
+	image = love.graphics.newImage("rs/textMaker/tM_textbox.png"),
 	r = 0,
 	g = 1,
 	b = 0,
 	a = 0.5,
-	image = love.graphics.newImage("rs/textMaker/tM_textbox.png"),
-	X = (256 / 2),
-	Y = (224 / 2),
 	placementOfY = { bottom = 176, top = 48 },
 	isAtBottom = true,
-	speed = 20,
-	lines = {
+	typewriter_speed = 20,
+	typewriter_lines = {
 		"Whats so green about this...",
-		"Docet ipsum dolor sit amet",
+		"Lorem ipsum dolor sit amet",
 		"Morbi vestibulum euismod",
 		"Sed malesuada augue lacus",
 	},
-	linesIndex = 1,
-	text_shown = "",
-	letterCount = 1,
+	typewriter_font = love.graphics.newFont("rs/fonts/earlyGameBoy.ttf", 8),
+	typewriter_linesIndex = 1,
+	typewriter_shownText = "",
+	typewriter_letterCount = 1,
 	indicator_image = love.graphics.newImage("rs/textMaker/tM_indicator.png"),
 	indicator_quads = { love.graphics.newQuad(0, 0, 9, 7, 9, 14), love.graphics.newQuad(0, 7, 9, 7, 9, 14) },
 	indicator_index = 1,
@@ -27,19 +27,19 @@ textbox_class = {
 	indicator_isShowing = false,
 }
 
-textbox_class.text_full = textbox_class.lines[textbox_class.linesIndex]
+textbox_class.text_full = textbox_class.typewriter_lines[textbox_class.typewriter_linesIndex]
 textbox_class.oX = math.floor(textbox_class.image:getWidth() / 2)
 textbox_class.oY = math.floor(textbox_class.image:getHeight() / 2)
 
 function textbox_class:setIn(bool)
-	self.linesIndex = 1
+	self.typewriter_linesIndex = 1
 	self.isIn = bool
 end
 
 function textbox_class:next()
-	self.letterCount = 1
-	if self.lines[self.linesIndex + 1] ~= nil then
-		self.linesIndex = self.linesIndex + 1
+	self.typewriter_letterCount = 1
+	if self.typewriter_lines[self.typewriter_linesIndex + 1] ~= nil then
+		self.typewriter_linesIndex = self.typewriter_linesIndex + 1
 	else
 		self:setIn(false)
 	end
@@ -47,7 +47,7 @@ end
 
 function textbox_class:update(dt)
 	if self.isIn then
-		self.text_full = self.lines[self.linesIndex]
+		self.text_full = self.typewriter_lines[self.typewriter_linesIndex]
 		-- Textbox placement
 		if self.isAtBottom then
 			self.Y = self.placementOfY.bottom
@@ -56,11 +56,11 @@ function textbox_class:update(dt)
 		end
 
 		-- Text
-		if self.letterCount < #self.text_full then
+		if self.typewriter_letterCount < #self.text_full then
 			self.indicator_isShowing = false
 			self.indicator_index = 1
 			self.indicator_timer = 1
-			self.letterCount = self.letterCount + (self.speed * dt)
+			self.typewriter_letterCount = self.typewriter_letterCount + (self.typewriter_speed * dt)
 		else
 			-- Animating the indicator
 			self.indicator_timer = self.indicator_timer + (4 * dt)
@@ -77,17 +77,17 @@ function textbox_class:update(dt)
 			self.indicator_isShowing = true
 		end
 
-		self.text_shown = string.sub(self.text_full, 1, self.letterCount)
+		self.typewriter_shownText = string.sub(self.text_full, 1, self.typewriter_letterCount)
 	end
 end
 
 function textbox_class:keypressed(key)
 	if self.isIn then
-		if key == "rshift" and self.letterCount < #self.text_full then
-			self.letterCount = #self.text_full
+		if key == "rshift" and self.typewriter_letterCount < #self.text_full then
+			self.typewriter_letterCount = #self.text_full
 		end
 
-		if key == "return" and self.letterCount >= #self.text_full then
+		if key == "return" and self.typewriter_letterCount >= #self.text_full then
 			self:next()
 		end
 	end
@@ -97,7 +97,7 @@ function textbox_class:draw()
 	if self.isIn then
 		love.graphics.draw(self.image, self.X, self.Y, 0, 1, 1, self.oX, self.oY)
 		love.graphics.setColor(self.r, self.g, self.b, self.a)
-		love.graphics.print(self.text_shown, textbox_class.font, ((self.X - self.oX) + 9), ((self.Y - self.oY) + 9))
+		love.graphics.print(self.typewriter_shownText, textbox_class.typewriter_font, ((self.X - self.oX) + 9), ((self.Y - self.oY) + 9))
 		love.graphics.setColor(1, 1, 1, 1)
 		if self.indicator_isShowing then
 			love.graphics.draw(
@@ -113,12 +113,12 @@ end
 local metaBox = {}
 metaBox.__index = textbox_class
 
-function textbox_class.new(bottom, speed, lines, r, g, b, a)
+function textbox_class.new(bottom, typewriter_speed, typewriter_lines, r, g, b, a)
 	local textInstance = setmetatable({}, metaBox)
 
 	textInstance.isAtBottom = bottom == true
-	textInstance.speed = speed or 2
-	textInstance.lines = lines or textbox_class.lines
+	textInstance.typewriter_speed = typewriter_speed or 2
+	textInstance.typewriter_lines = typewriter_lines or textbox_class.typewriter_lines
 	textInstance.r = r or 1
 	textInstance.g = g or 1
 	textInstance.b = b or 1
